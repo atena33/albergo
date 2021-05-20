2e743c9779796eb73ea1dbcf0655fb4649f2a2d8
Anna <annamura833@gmail.com>
2021-05-20 17:28:48 +0200

    js

--- main.js
+++ main.js
@@ -2,3 +2,46 @@
 
     alert('Benvenuti al mio hotel!!');
 }
+
+function save() {
+
+    var username = document.getElementById('username').value;
+
+    localStorage.setItem('username', username);
+}
+
+
+function check() {
+
+   
+
+    var getUsername = localStorage.getItem('username');
+    
+    
+    return getUsername;
+}
+
+function disabled() {
+
+    var submit = document.getElementById('submit').disabled = true;
+
+}
+
+function enabled (){
+
+    var submit = document.getElementById('submit').disabled = false;
+
+}
+
+function redirect() {
+
+    location.href = "HomePage";
+
+}
+
+function logout() {
+
+    localStorage.clear();
+    location.href = "Login.html";
+
+}
